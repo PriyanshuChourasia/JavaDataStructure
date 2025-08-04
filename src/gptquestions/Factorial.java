16dceaf1da3c70b4c9f0352f55fb3a6283c92404
Priyanshu Chourasia <priyanshuchourasia916@gmail.com>
2025-08-04 10:30:03 +0530

    Done upto factorial

--- src/gptquestions/Factorial.java
+++ src/gptquestions/Factorial.java
@@ -1,7 +1,22 @@
 package gptquestions;
 
+import java.util.Scanner;
+
 public class Factorial {
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number and get Factorial: ");
+        int num = sc.nextInt();
 
+        int i=1;
+        int mul = 1;
+        while(i<=num){
+            mul = mul * i;
+            i++;
+        }
+
+        System.out.println("Factorial : "+mul);
+
+        sc.close();
     }
 }
